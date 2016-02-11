f8fdbf07a96a2628737bad9ec64d6d817074042d
nsteffens <n.steffens@me.com>
2016-02-11 02:57:27 +0100

    Initial Commit

---
+++ ownGPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ownGPS
+//
+//  Created by Nico Steffens on 11.02.16.
+//  Copyright © 2016 Nico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
